ec42c41fda5d919a47e4c1653f962cc7b006a496
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-08-05 14:43:38 +0100

    - Updated the dependency on Date::Utils v0.21.

--- lib/Date/Persian/Simple.pm
+++ lib/Date/Persian/Simple.pm
@@ -1,6 +1,6 @@
 package Date::Persian::Simple;
 
-$Date::Persian::Simple::VERSION   = '0.12';
+$Date::Persian::Simple::VERSION   = '0.13';
 $Date::Persian::Simple::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Date::Persian::Simple - Represents Persian date.
 
 =head1 VERSION
 
-Version 0.12
+Version 0.13
 
 =cut
 
